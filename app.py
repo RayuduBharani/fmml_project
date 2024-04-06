780a23118bdda3e8a7c040455473dd110aa57080
Anil sai <anilsainunnagamil.com@Anils-MacBook-Air.local>
2024-04-07 01:30:17 +0530

    initial commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import time
 
 app = flask.Flask(__name__, template_folder='templates')
 
@@ -9,46 +10,35 @@ df2 = pd.read_csv('./model/tmdb.csv')
 
 tfidf = TfidfVectorizer(stop_words='english',analyzer='word')
 
-#Construct the required TF-IDF matrix by fitting and transforming the data
 tfidf_matrix = tfidf.fit_transform(df2['soup'])
 print(tfidf_matrix.shape)
 
-#construct cosine similarity matrix
 cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 print(cosine_sim.shape)
 
 df2 = df2.reset_index()
 indices = pd.Series(df2.index, index=df2['title']).drop_duplicates()
 
-# create array with all movie titles
 all_titles = [df2['title'][i] for i in range(len(df2['title']))]
 
 def get_recommendations(title):
     global sim_scores
-    # Get the index of the movie that matches the title
     idx = indices[title]
-    # Get the pairwise similarity scores of all movies with that movie
     sim_scores = list(enumerate(cosine_sim[idx]))
-    # Sort the movies based on the similarity scores
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    # Get the scores of the 10 most similar movies
     sim_scores = sim_scores[1:11]
-    # print similarity scores
     print("\n movieId      score")
     for i in sim_scores:
         print(i)
 
-    # Get the movie indices
     movie_indices = [i[0] for i in sim_scores]
 
-    # return list of similar movies
     return_df = pd.DataFrame(columns=['Title','Homepage'])
     return_df['Title'] = df2['title'].iloc[movie_indices]
     return_df['Homepage'] = df2['homepage'].iloc[movie_indices]
     return_df['ReleaseDate'] = df2['release_date'].iloc[movie_indices]
     return return_df
 
-# Set up the main route
 @app.route('/', methods=['GET', 'POST'])
 
 def main():
@@ -57,7 +47,6 @@ def main():
             
     if flask.request.method == 'POST':
         m_name = " ".join(flask.request.form['movie_name'].split())
-#        check = difflib.get_close_matches(m_name,all_titles,cutout=0.50,n=1)
         if m_name not in all_titles:
             return(flask.render_template('notFound.html',name=m_name))
         else:
@@ -75,7 +64,29 @@ def main():
                 
 
             return flask.render_template('found.html',movie_names=names,movie_homepage=homepage,search_name=m_name, movie_releaseDate=releaseDate, movie_simScore=sim_scores)
+        
+    
+def get_genre(name):
+    movies = df2[df2['genres'].str.contains(name)]
+    movies_data = movies.sample(10)
+    return movies_data
+
+
+@app.route('/genre/<genre_name>')
+def fetch_genre(genre_name):
+    movies_homepage = []
+    movie_names = []
+    release_date = []
+    movies_data = get_genre(genre_name)
+    for i in range(len(movies_data)):
+        movie_names.append(movies_data.iloc[i][8])
+        release_date.append(movies_data.iloc[i][13])
+        url = movies_data.iloc[i][4]
+        if len(str(url)) > 3:
+            movies_homepage.append(str(url))
+        else:
+            movies_homepage.append('')
+    return flask.render_template('genres.html', genre_name = genre_name, movie_homepage = movies_homepage, movie_releaseDate = release_date, movie_names = movie_names)
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
-    #app.run()
